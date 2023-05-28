507ecdbb8c163d3f3e7083e5a6ad7913b494a7fe
muhammadali <134692695+muhammadjanovali717@users.noreply.github.com>
2023-05-28 16:05:00 +0500

    Add files via upload

---
+++ counter/script.js
@@ -0,0 +1,10 @@
+let count = 0
+function incremnd(){
+    count = count +1;
+    document.querySelector('.in').innerHTML = count
+}
+
+function dec(){
+    count = count -1;
+    document.querySelector('.in').innerHTML = count
+}
